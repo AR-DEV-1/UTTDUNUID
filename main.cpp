e1eb191fb5070bc9ca4f472a0a3f07d073210f36
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2024-01-31 19:46:01 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main() {
     std::cout << "Token: " << AppUtils::generateToken() << std::endl;
     std::cout << "Current Time: " << AppUtils::getCurrentTime() << std::endl;
     std::cout << "Current Day: " << AppUtils::getCurrentDay() << std::endl;
-    std::cout << "Random Username: " << AppUtils::generateRandomUsername() << std::endl;
-    std::cout << "Random User ID: " << AppUtils::generateRandomUserId() << std::endl;
+    std::cout << "Username: " << AppUtils::generateRandomUsername() << std::endl;
+    std::cout << "User ID: " << AppUtils::generateRandomUserId() << std::endl;
 
     return 0;
 }
