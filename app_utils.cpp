38958912e66caa396d13cf229f1ca5e9781f5e22
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2024-01-31 19:45:00 +0500

    Update app_utils.cpp

--- app_utils.cpp
+++ app_utils.cpp
@@ -1,6 +1,6 @@
 // app_utils.cpp
 
-#include "app_utils.h"
+#include "include/app_utils.h"
 #include <iostream>
 #include <chrono>
 #include <iomanip>
@@ -9,17 +9,13 @@
 #include <ctime>
 
 std::string AppUtils::generateUUID() {
-    // Implement UUID generation logic (you may use a library or a custom algorithm)
-    // For simplicity, let's just use a random string for illustration
+    
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
 }
 
 std::string AppUtils::generateToken() {
-    // Implement token generation logic (you may use a library or a custom algorithm)
-    // For simplicity, let's just use a random string for illustration
     return "random_token";
 }
-
 std::string AppUtils::getCurrentTime() {
     auto now = std::chrono::system_clock::now();
     std::time_t now_c = std::chrono::system_clock::to_time_t(now);
@@ -39,13 +35,10 @@ std::string AppUtils::getCurrentDay() {
 }
 
 std::string AppUtils::generateRandomUsername() {
-    // Implement random username generation logic
-    // For simplicity, let's use a fixed prefix and a random number
     return "user_" + std::to_string(generateRandomUserId());
 }
 
 int AppUtils::generateRandomUserId() {
-    // Implement random user ID generation logic
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dist(1000, 9999);
